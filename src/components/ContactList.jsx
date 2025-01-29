c37e3e89d41534cb8b6df798571457f3b861d099
Tomas Toledo <trtoledo1977@gmail.com>
2025-01-29 02:18:44 -0300

    console.log(data)

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -2,17 +2,23 @@ import React, {useEffect, useState} from "react";
 import ContactRow from "./ContactRow/ContactRow";
 import axios from "axios";
 
-const dummyContacts = [
-    { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-    { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-    { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-  ];
+// const dummyContacts = [
+//     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+//     { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+//     { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+//   ];
 
 function ContactList() { 
-  const [contacts, setContacts] = useState(dummyContacts);
+  const [contacts, setContacts] = useState([]);
   useEffect(() => {
     axios("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users")
-    .then((data) => console.log(data))
+    .then((data) => {
+      {
+        console.log(data);
+        console.log(data.data);
+        setContacts(data.data);
+      }
+  })
     .catch((err) => console.log(err));
     
   }, [])
